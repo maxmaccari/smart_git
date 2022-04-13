d2851ca63b8a7bf53c0dbf79b615e5de69a9304e
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-04-12 21:50:56 -0400

    Implement buttons list

--- test/smart_git_web/live/page/page_live_test.exs
+++ test/smart_git_web/live/page/page_live_test.exs
@@ -7,5 +7,10 @@ defmodule SmartGitWeb.PageLiveTest do
     {:ok, view, _html} = live(conn, Routes.page_path(conn, :index))
 
     assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Elixir")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Ruby")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Javascript")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Rust")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "F#")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Elm")
   end
 end
